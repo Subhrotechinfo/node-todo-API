04113b643cc851b0f5576a7f066a171bd4d9bb6b
Subhrotechinfo <subhro.techinfo@gmail.com>
2018-08-15 21:53:55 +0530

    Test POST /todos route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-//use build in library
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoAppMongoose');
 
--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
-var User = mongoose.model('User',{
-    email:{
-      type:String,
-      required:true,
-      minlength:1,
-      trim:true
-    }
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-module.exports = {User};
+module.exports = {User}
