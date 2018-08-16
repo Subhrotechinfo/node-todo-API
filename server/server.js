ae004a99933c63afd9433e9aa530ebdebc254dd8
Subhrotechinfo <subhro.techinfo@gmail.com>
2018-08-16 11:46:09 +0530

    Add test for GET /todos

--- server/server.js
+++ server/server.js
@@ -30,11 +30,8 @@ app.get('/todos' , (req,res) => {
       res.send({todos});
   }, (e) =>{
       res.status(400).send(e);
-  })
+  });
 });
-
-
-
 
 app.listen(3000,()=>{
   console.log('Started on port 3000. ');
