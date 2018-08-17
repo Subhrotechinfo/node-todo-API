dc14766dbd8404aa513659a6378bd8c3ce9ce632
Subhrotechinfo <subhro.techinfo@gmail.com>
2018-08-17 12:55:03 +0530

    added localdb URL in mongoose.js file

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ console.log('environment',env);
 
 if(env === 'development'){
   process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppMongoose';
+  process.env.MONGODB_URI = 'mongodb://username:password@ds123532.mlab.com:23532/todoappmongoose';
 }else if(env === 'test'){
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppMongoose'
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,5 +5,5 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 //mongodb://localhost:27017/TodoAppMongoose
 //mongodb://<dbuser>:<dbpassword>@ds123852.mlab.com:23852/todoapi
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env. || 'mongodb://localhost:27017/TodoAppMongoose');
 module.exports = {mongoose};
